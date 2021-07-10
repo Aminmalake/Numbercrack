d28acd445fff03db044afdfd63de8b1d93dace1b
Aminmalake <66966944+Aminmalake@users.noreply.github.com>
2021-07-10 23:50:37 +0300

    numbercrack.py
    https://www.instagram.com/i4m.amin

--- numbercrack.py
+++ numbercrack.py
@@ -1,7 +1,7 @@
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
 for n in range(100000):
-    nmbr = random.randint(1111111, 9999999)
+    nmbr = random.randint(2222222, 9876543)
     sys.stdout = open('.txt', 'a')
     print nmbr
     sys.stdout.flush()
